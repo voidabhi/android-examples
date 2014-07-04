0454276230a5dd50079e083247f237d3e39ea300
Abhijeet <void.aby@gmail.com>
2014-07-04 23:48:09 +0530

    added ActionTabEXample

---
+++ ActionTabExample/src/com/example/actiontabexample/DummySectionFragment.java
@@ -0,0 +1,41 @@
+package com.example.actiontabexample;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A dummy fragment representing a section of the app, but that simply
+ * displays dummy text.
+ */
+public class DummySectionFragment extends Fragment {
+	/**
+	 * The fragment argument representing the section number for this
+	 * fragment.
+	 */
+	public static final String ARG_SECTION_NUMBER = "section_number";
+
+	public DummySectionFragment() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		
+		// Fragment context (analogy to applicationContext in n activity)
+		// - layout of fragment
+		// - container of fragment in activity
+		View rootView = inflater.inflate(R.layout.fragment_main_dummy,
+				container, false);
+		// - Getting ui element from the fragment context
+		TextView dummyTextView = (TextView) rootView
+				.findViewById(R.id.section_label);
+		dummyTextView.setText(Integer.toString(getArguments().getInt(
+				ARG_SECTION_NUMBER)));
+		// returning fragment context
+		return rootView;
+	}
+}
