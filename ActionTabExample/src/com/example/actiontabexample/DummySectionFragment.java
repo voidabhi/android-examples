acca86b6a6882508aa24485b90c70e766bd44466
Abhijeet Mohan <void.aby@gmail.com>
2014-10-21 22:42:52 +0530

    typo

--- ActionTabExample/src/com/example/actiontabexample/DummySectionFragment.java
+++ ActionTabExample/src/com/example/actiontabexample/DummySectionFragment.java
@@ -25,7 +25,7 @@ public class DummySectionFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		
-		// Fragment context (analogy to applicationContext in n activity)
+		// Fragment context (analogy to applicationContext in an activity)
 		// - layout of fragment
 		// - container of fragment in activity
 		View rootView = inflater.inflate(R.layout.fragment_main_dummy,
