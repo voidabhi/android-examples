9cf43647c62c2cd312d359f5e99894fb8293fa39
Abhijeet Mohan <void.aby@gmail.com>
2014-11-14 21:41:15 +0530

    Update SplashScreenActivity.java

--- SplashScreenExample/src/com/example/splashscreenexample/SplashScreenActivity.java
+++ SplashScreenExample/src/com/example/splashscreenexample/SplashScreenActivity.java
@@ -42,6 +42,7 @@ public class SplashScreenActivity extends Activity {
       // do not worry about this Thread.sleep
       // this is an async task, it will not disrupt the UI
       try {
+        // Holding thread 
         Thread.sleep(SPLASH_SHOW_TIME);
       } catch (InterruptedException e) {
         e.printStackTrace();
