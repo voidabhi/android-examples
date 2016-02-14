89b250c4a490e69a59a3bdfe554ee8b32f24b906
Abhijeet Mohan <void.aby@gmail.com>
2016-02-14 22:43:36 +0530

    Update FirstFragment.java

--- NavigationDrawerExample/src/com/example/navigationdrawerexample/FirstFragment.java
+++ NavigationDrawerExample/src/com/example/navigationdrawerexample/FirstFragment.java
@@ -4,11 +4,21 @@
 package com.example.navigationdrawerexample;
 
 import android.support.v4.app.Fragment;
+import android.views.View;
+import android.views.ViewGroup;
+import android.views.LayoutInflater;
+import android.os.Bundle;
 
 /**
  * @author ABHIJEET
  *
  */
 public class FirstFragment extends Fragment {
+  
+   public View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            super.onCreateView(inflater, container, savedInstanceState);          
+            return view;
+        }
 
 }
